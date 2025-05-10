37bfacc42c3154b798a2765a5014cb31cab5c39d
Javier <javiermonsalvesm@gmail.com>
2025-05-10 11:50:44 -0400

    SP2- comentarios en panel

--- frontend/src/pages/PanelJefe.jsx
+++ frontend/src/pages/PanelJefe.jsx
@@ -7,7 +7,10 @@ export default function PanelJefe() {
       <h1 className="text-2xl font-bold">Bienvenido, Jefe de Ventas</h1>
       <p className="mt-2 text-gray-600">Selecciona una opción en el menú para comenzar.</p>
       <Productos/>  
-      <FormularioProducto/>  
+      
             </div>
   );
 }
+
+
+//<FormularioProducto/>   -->  tabla pero no en tiempo real.  
